e469f242a3b95de95c29e8d70693b919feb1f6e6
avinash24112007 <avinash24112007@gmail.com>
2026-04-25 20:07:07 +0530

    feat: add get_current_app function to retrieve the active application name

---
+++ src/utils/gui_utils.py
@@ -0,0 +1,33 @@
+import win32gui, psutil, win32process
+
+def get_current_app():
+    try:
+
+        app_name = {
+            'msedge': 'edge',
+            'chrome': 'chrome',
+            'explorer': 'explorer',
+            'spotify': 'spotify',
+
+        }
+        # 1. Get the active window ID
+        hwnd = win32gui.GetForegroundWindow()
+        
+        # 2. Get the Process ID (PID) tied to that window
+        # THIS is where win32process is used!
+        _, pid = win32process.GetWindowThreadProcessId(hwnd)
+        
+        # 3. Get the actual .exe name using the PID
+        process = psutil.Process(pid)
+        exe_name = process.name().lower()  
+        
+        # 4. Your clean 'if' logic
+        for process_name, return_name in app_name.items():
+            if process_name in exe_name:
+                return return_name
+        
+        return exe_name 
+        
+    except Exception:
+        return None
+    

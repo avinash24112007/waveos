6bed49aacaa61de46a5d09e3f6fd3d286f7362d2
avinash24112007 <avinash24112007@gmail.com>
2026-04-25 20:04:20 +0530

    Seperate tf and onnx prediction functions

--- src/commands/profiles.py
+++ src/commands/profiles.py
@@ -1,5 +1,5 @@
 from src.commands.executor import next_tab, prev_tab, refresh, scroll_down, scroll_up,  go_back
-
+from src.commands.executor import next_track, prev_track, pause_play, volume_up, volume_down, screenshot
 CHROME_PROFILE = {
     'SWIPE_RIGHT': next_tab,
     'SWIPE_LEFT': prev_tab,
@@ -7,4 +7,19 @@ CHROME_PROFILE = {
     'FIST': go_back,
     'OPEN_PALM': scroll_up,
     'PEACE': scroll_down,
+}
+
+
+SPOTIFY_PROFILE = {
+    'SWIPE_RIGHT': next_track,
+    'SWIPE_LEFT': prev_track,
+    'FIST': pause_play,
+    'OPEN_PALM': volume_up,
+    'PEACE': volume_down,
+}
+
+DEFAULT_PROFILE = {
+    'OPEN_PALM': volume_up,
+    'PEACE': volume_down,
+    'FIST': screenshot 
 }
--- src/detection/action_detect.py
+++ src/detection/action_detect.py
@@ -3,13 +3,13 @@ from src.utils.keypoint_utils import extract_keypoints
 import numpy as np
 
 
-def predict_motion_pose(sequence, model, actions):
+def predict_motion_pose_tf(sequence, model, actions):
 
 
     if len(sequence) == 30:
         model_input = np.expand_dims(np.array(sequence), axis=0)
 
-        action_result = model.predict(model_input)
+        action_result = model.predict(model_input, verbose = 0)
 
         best_index = int(np.argmax(action_result))
         best_label = actions[best_index]
@@ -18,3 +18,27 @@ def predict_motion_pose(sequence, model, actions):
         return best_label, confidence 
     
     return None, 0.0
+
+import onnxruntime as rt
+import numpy as np
+import sys, os
+
+BASE_DIR = os.path.dirname(sys.executable) if getattr(sys, 'frozen', False) else os.path.dirname(os.path.abspath(__file__))
+
+action_session = rt.InferenceSession(os.path.join(BASE_DIR, 'models', 'action_model.onnx'))
+def predict_motion_pose(sequence, actions, threshold=0.8):
+    if len(sequence) < 30:
+        return None, 0.0
+    
+    model_input = np.expand_dims(np.array(sequence), axis=0).astype(np.float32)
+    
+    input_name = action_session.get_inputs()[0].name
+    model_result = action_session.run(None, {input_name: model_input})[0]
+    
+    confidence = np.max(model_result)# type: ignore
+    if confidence < threshold:
+        return None, confidence
+    
+    best_index = np.argmax(model_result)# type: ignore
+    best_label = actions[best_index]
+    return best_label, confidence

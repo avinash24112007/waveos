39cae74235f12dbc3f96cb2f5004fb6864ddea82
avinash24112007 <avinash24112007@gmail.com>
2026-04-25 20:06:59 +0530

    refactor: rename and enhance static pose prediction functions for clarity and performance

--- src/detection/static_detect.py
+++ src/detection/static_detect.py
@@ -3,13 +3,34 @@ from src.utils.keypoint_utils import extract_keypoints
 import numpy as np
 
 
-def predict_static_pose(keypoints, model, actions):
+def predict_static_pose_tf(keypoints, model, actions):
 
     model_input = np.expand_dims(np.array(keypoints), axis=0)
 
-    model_result = model.predict(model_input)
+    model_result = model.predict(model_input, verbose = 0)
     best_index = int(np.argmax(model_result))
     best_label = actions[best_index]
     confidence = np.max(model_result)
     
     return best_label, confidence 
+
+import onnxruntime as rt
+import numpy as np
+import sys, os
+
+BASE_DIR = os.path.dirname(sys.executable) if getattr(sys, 'frozen', False) else os.path.dirname(os.path.abspath(__file__))
+
+static_session = rt.InferenceSession(os.path.join(BASE_DIR, 'models', 'static_model.onnx'))
+def predict_static_pose(keypoints, actions, threshold=0.9):
+    model_input = np.expand_dims(np.array(keypoints), axis=0).astype(np.float32)
+    
+    input_name = static_session.get_inputs()[0].name
+    model_result = static_session.run(None, {input_name: model_input})[0]
+    
+    confidence = np.max(model_result)# type: ignore
+    if confidence < threshold:
+        return None, confidence
+    
+    best_index = np.argmax(model_result)# type: ignore
+    best_label = actions[best_index]
+    return best_label, confidence

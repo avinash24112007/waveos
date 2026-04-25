7ba7c3e771451067a660dcbb4c453af323526d21
avinash24112007 <avinash24112007@gmail.com>
2026-04-25 20:02:20 +0530

    refactor: streamline command functions and improve GUI detection logic

--- src/commands/executor.py
+++ src/commands/executor.py
@@ -1,27 +1,68 @@
-import pyautogui
+import pyautogui, time
 
+from src.utils.gui_utils import get_current_app
 
+def volume_up():
+    pyautogui.press('volumeup')
+
+def volume_down():
+    pyautogui.press('volumedown')
+
+# Chrome/ Edge profile
+def scroll_up(): pyautogui.scroll(30)
+
+def scroll_down(): pyautogui.scroll(-30)
+
+def refresh():  pyautogui.hotkey('ctrl', 'r')
+
+def go_back(): pyautogui.hotkey('alt', 'left')
 
 def next_tab(): pyautogui.hotkey('ctrl', 'shift', 'tab')
 
 def prev_tab(): pyautogui.hotkey('ctrl', 'tab')
 
-def volume_up():
-    from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
-    from ctypes import cast, POINTER
-    from comtypes import CLSCTX_ALL
+
+#Spotify Profiles
+def next_track(): pyautogui.hotkey('ctrl', 'right')
+
+def prev_track(): pyautogui.hotkey('ctrl', 'left')
+
+def pause_play(): pyautogui.hotkey('space')
+
+def screenshot(): pyautogui.hotkey('win', 'shift', 's')
+
+
+last_executed_cmd = 0
+COOLDOWN_CMD = 1.0
+
+def execute_command(gesture, profile):
+    global last_executed_cmd
+
+    now = time.time()
+
+    if now - last_executed_cmd < COOLDOWN_CMD:
+        return
+
+    command = profile.get(gesture)
+    if command:
+        command()
+        last_executed_cmd = now
+        
+
+last_executed_GUI = 0.0
+COOLDOWN_GUI = 1.0
+last_known_app = None
+def detect_change_gui():
+
+    global last_executed_GUI, last_known_app
+
+    now = time.time()
+
+
+    if now - last_executed_GUI < COOLDOWN_GUI:
+        return last_known_app
     
-    devices = AudioUtilities.GetSpeakers()
-    interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)  # type: ignore
-    volume = cast(interface, POINTER(IAudioEndpointVolume))
-    current_vol = volume.GetMasterVolumeLevelScalar()  # type: ignore
-    volume.SetMasterVolumeLevelScalar(min(1.0, current_vol + 0.1), None)  # type: ignore
+    last_known_app = get_current_app()
+    last_executed_GUI = now
+    return last_known_app
 
-
-def scroll_up(): pyautogui.scroll(3)
-
-def scroll_down(): pyautogui.scroll(-3)
-
-def refresh():  pyautogui.hotkey('ctrl', 'r')
-
-def go_back(): pyautogui.hotkey('alt', 'left')
